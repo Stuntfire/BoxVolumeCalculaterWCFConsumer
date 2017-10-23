3bdff6ee92ffac47e1add35b4e16a2209ec79349
Søren Roed Schack <soer622t@edu.easj.dk>
2017-10-23 12:54:05 +0200

    Add project files.

---
+++ BoxVolumeCalculaterWCFConsumer/Calculator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxVolumeCalculaterWCFConsumer
+{
+    [DataContract]
+    public class Claculator
+    {
+        private double _length;
+        [DataMember]
+        public double Length
+        {
+            get { return _length; }
+            set { _length = value; }
+        }
+
+        private double _width;
+        [DataMember]
+        public double Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+
+        private double _height;
+        [DataMember]
+        public double Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+        private double _side1;
+        [DataMember]
+        public double Side1
+        {
+            get { return _side1; }
+            set { _side1 = value; }
+        }
+
+        private double _side2;
+        [DataMember]
+        public double Side2
+        {
+            get { return _side2; }
+            set { _side2 = value; }
+        }
+
+        private double _volume;
+        [DataMember]
+        public double Volume
+        {
+            get { return _volume; }
+            set { _volume = value; }
+        }
+
+
+        public Claculator()
+        {
+
+        }
+
+        public Claculator(double lenght, double width, double height)
+        {
+            this.Length = lenght;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(_length)}: {_length}, {nameof(_width)}: {_width}, {nameof(_height)}: {_height}, {nameof(_side1)}: {_side1}, {nameof(_side2)}: {_side2}";
+
+            //return string.Format("Volume: {0}", _volume);
+        }
+    }
+}
---
+++ BoxVolumeCalculaterWCFConsumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BoxVolumeCalculaterWCFConsumer.ServiceReference1;
+
+namespace BoxVolumeCalculaterWCFConsumer
+{
+    class Program
+    {
+        /// <summary>
+        /// link til wsdl-filen på Azure: http://boxvolumecalculaterwcfazure.azurewebsites.net/Service1.svc?wsdl
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            using (var consumerVolume = new Service1Client("BasicHttpBinding_IService1"))
+            {
+                consumerVolume.GetVolume(2, 2, 2);
+                Console.WriteLine(consumerVolume);
+            }
+
+            using (var consumerSide = new Service1Client("BasicHttpBinding_IService1"))
+            {
+                consumerSide.GetSide(8, 2, 2);
+                Console.WriteLine(consumerSide);
+            }
+        }
+    }
+}
